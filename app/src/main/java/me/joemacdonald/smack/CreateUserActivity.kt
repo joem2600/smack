107747c131b1dac90f8ce0967e367b9273702674
JoeM <joem2525@gmail.com>
2017-12-29 17:56:55 -0500

    Lesson 5 completed (70) - Enabled user profile tap and generate background color button

---
+++ app/src/main/java/me/joemacdonald/smack/CreateUserActivity.kt
@@ -0,0 +1,59 @@
+package me.joemacdonald.smack
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
+
+class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar : String = "profileDefault"
+    var avatarColour = "[0.5, 0.5, 0.5, 1]"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+        val random = Random()
+        val colour = random.nextInt(2)
+        val avatar = random.nextInt(28)
+
+        userAvatar = when (colour == 0) {
+            true -> "light$avatar"
+            false -> "dark$avatar"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImg.setImageResource(resourceId)
+
+    }
+
+    fun backgroundColorBtnClicked(view : View) {
+
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarImg.setBackgroundColor(Color.rgb(r, g, b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = r.toDouble() / 255
+        val savedB = r.toDouble() / 255
+
+        avatarColour = "[$savedR, $savedG, $savedB]"
+        Log.d("JOEMAC", " myColour $avatarColour")
+
+    }
+
+    fun createUserBtnClicked(view : View) {
+
+    }
+
+}
